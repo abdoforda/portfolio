b1382e988003895dbb93f481ca78d1143c3cc6f6
abdoforda <abdelrahmaan3@gmai.com>
2026-04-20 10:58:25 +0200

    a

--- webhook.php
+++ webhook.php
@@ -1,7 +1,18 @@
 <?php
 
-$output = [];
-exec('/home3/ahmadothman/deploy.sh 2>&1', $output);
+$secret = "mysecret123"; // نفس اللي هنحطه في GitHub
 
-echo "<pre>";
-print_r($output);
+$content = file_get_contents("php://input");
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? '';
+
+$hash = 'sha1=' . hash_hmac('sha1', $content, $secret);
+
+if (!hash_equals($hash, $signature)) {
+    http_response_code(403);
+    exit('Invalid signature');
+}
+
+// شغل deploy
+exec('/home3/ahmadothman/deploy.sh 2>&1');
+
+echo "Deployed";
